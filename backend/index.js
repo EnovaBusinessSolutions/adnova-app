5be8e4440da650569a9fe382cd51e1ece5d5b28d
Jose Manuel Mejia <josemanuelmejiadominguez2@gmail.com>
2025-06-19 16:51:36 -0600

    sss

--- backend/index.js
+++ backend/index.js
@@ -405,8 +405,8 @@ app.get('/connector/interface', (req, res) => {
 });
 
 // ✅ Captura las rutas embebidas de Shopify y redirige
-app.get(`/apps/${process.env.SHOPIFY_APP_HANDLE}/*`, (req, res) => {
-  const subPath = req.originalUrl.replace(`/apps/${process.env.SHOPIFY_API_KEY}`, '');
+app.use(`/apps/${process.env.SHOPIFY_APP_HANDLE}`, (req, res) => {
+  const subPath = req.originalUrl.replace(`/apps/${process.env.SHOPIFY_APP_HANDLE}`, '');
   return res.redirect(`/connector${subPath}`);
 });
 
