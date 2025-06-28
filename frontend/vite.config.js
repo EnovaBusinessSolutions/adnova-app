5e4f434fd3e67f200376e6ac5e273340ac0d268a
Jose Manuel Mejia <josemanuelmejiadominguez2@gmail.com>
2025-06-28 12:55:38 -0600

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,15 @@
-// frontend/vite.config.js
+
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.',                       // raíz de "frontend"
+  root: '.',                       
   build: {
-    outDir: '../public/connector', // deja el bundle junto a tu interface.html
-    emptyOutDir: false,            // no borra otros archivos en la carpeta destino
+    outDir: '../public/connector', 
+    emptyOutDir: false,            
     rollupOptions: {
-      input: 'interface.js',     // el único archivo de entrada (entry)
+      input: 'interface.js',     
       output: {
-        entryFileNames: 'interface.bundle.js' // nombre fijo para que no tengas que editar el HTML nunca
+        entryFileNames: 'interface.bundle.js' 
       }
     }
   }
