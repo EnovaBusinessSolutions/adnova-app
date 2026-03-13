9ac576d12611a1e9a5138e94250b430b83cc3b1b
Germán <german.mm.2001@gmail.com>
2026-03-12 22:46:56 -0600

    chore: manually revert public/adray-analytics.html to 0d1aa6fc958cb0fcc108fb13ba6ec436196d2030

--- fix2.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let text = fs.readFileSync('public/adray-analytics.html', 'utf8');
-
-text = text.replace(/Atribuci&oacute;n \([^\s]+ltimo Clic\)/g, 'Atribuci&oacute;n (&Uacute;ltimo Clic)');
-
-fs.writeFileSync('public/adray-analytics.html', text, 'utf8');
